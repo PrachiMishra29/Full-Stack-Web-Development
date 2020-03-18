7f957065bae8a0e4e871dc7e6596fa2d67f7ccac
PrachiMishra29 <pmishra859@gmail.com>
2020-03-18 21:55:36 +0530

    with delete feature

---
+++ CSE-B/Mid-Term/Question1/delete.php
@@ -0,0 +1,56 @@
+<?php
+include('config.php');
+?>
+<?php $id=$_GET['id'];
+?>
+
+<?php
+    $sql = "SELECT * FROM `details` WHERE id='$id'";
+    $result = mysqli_query($conn, $sql);
+    $data = $result->fetch_assoc();
+    $enrollment = $data['enrollment'];
+    $age = $data['age'];
+    $course = $data['course'];
+    $branch = $data['branch'];
+    $contact = $data['contact'];
+    $email = $data['email'];
+?>
+
+<?php
+if(isset($_POST['btn'])){
+    $enrollment = $_POST['enrollment'];
+    $age = $_POST['age'];
+    $course = $_POST['course'];
+    $branch = $_POST['branch'];
+    $contact = $_POST['contact'];
+    $email = $_POST['email'];
+    $sql = "DELETE FROM  `details`  WHERE id='$id'";
+    if(mysqli_query($conn, $sql)){
+        echo "Data deleted successfully...";
+    }
+    else{
+        echo "Deletion Failed...Try Again";
+    }
+}
+else{
+    echo "Please click delete button";
+}
+?>
+
+<html>
+    <head>
+    <title>Input Form</title>
+    </head>
+
+    <body>
+    <form action="delete.php?id=<?php echo $id ?>" method="post">
+    Enrollment No: <input type="number" name="enrollment" value="<?php echo $enrollment; ?>"><br>
+      Age:<input type="text" name="age"  value="<?php echo $age; ?>" placeholder="Enter Age"><br>
+      Course:<input type="text" name="course"  value="<?php echo $course; ?>" placeholder="Enter Course"><br>
+      Branch:<input type="text" name="branch"  value="<?php echo $branch; ?>" placeholder="Enter Branch"><br>
+      Contact:<input type="text" name="contact" value="<?php echo $contact; ?>" placeholder="Enter Contact"><br>
+      Email: <input type="email" name="email" value="<?php echo $email; ?>" placeholder="Enter Email"><br>
+      <input type="submit" name='btn' value="Delete">
+    </form>
+    </body>
+</html>
